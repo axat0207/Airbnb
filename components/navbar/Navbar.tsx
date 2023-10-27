a7294e859af225e5b82d5248a7cc8460123b7694
axat0207 <axxatagrawal@gmail.com>
2023-10-27 08:17:40 +0530

    category section added and working fine

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { Logo } from "./Logo";
 import Search from "./Search";
 import { UserMenu } from "./UserMenu";
 import { safeUser } from "@/app/types";
+import Categories from "./Categories";
 
 
 interface Navbarprops{
@@ -14,17 +15,32 @@ interface Navbarprops{
 export const Navbar:React.FC<Navbarprops> = ({currentUser}) => {
    // console.log(currentUser)
   return (
-    <div>
-      <div className="fixed w-full z-20 shadow-sm border-b-[1px] py-4">
-        <Container>
-          <div className="flex flex-row justify-between items-center gap-3 md:gap-0">
-            <Logo />
-            <Search />
-            <UserMenu currentUser={currentUser} />
-          </div>
-        </Container>
-      </div>
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
+      <Container>
+        <div 
+          className="
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
     </div>
+    <Categories />
+  </div>
   );
 };
 export default Navbar;
